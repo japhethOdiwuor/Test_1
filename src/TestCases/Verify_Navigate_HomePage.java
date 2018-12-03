b33de60f9be4e6afd1ed4747376df5555a384b66
ja4oti <ja4oti@gmail.com>
2018-12-03 14:42:51 +0300

    created test verify electronics present on Home Page

---
+++ src/TestCases/Verify_Navigate_HomePage.java
@@ -0,0 +1,29 @@
+package TestCases;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import LoginTest.Login;
+
+public class Verify_Navigate_HomePage extends Login {
+
+	@Test(priority = 4)
+	public void verify_Electronics_On_HomePage() {
+
+		String elementText = driver.findElement(By.xpath("//*[@id='container']/div/header/div[3]/ul")).getText().trim();
+		if (elementText.contains("Electronics")) {
+
+			System.out.println(elementText);
+			System.out.println("----------------------");
+			System.out.println("Successfully Verified ELECTRONICS present on Home Page");
+			System.out.println("----------------------");
+		} else {
+
+			System.out.println(elementText);
+			System.out.println("----------------------");
+			System.out.println("Failed to Verify ELECTRONICS present on Home Page");
+			System.out.println("----------------------");
+		}
+	}
+
+}
